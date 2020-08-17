d85d54f425c049c33579e97148fa1bb2aa82111b
Yossy Carmeli <ycarmeli@Lightricks.com>
2020-08-17 14:13:14 +0300

    Organise folders.
    Organise folders- Model,View,View Controller.

---
+++ Matchismo/View/PlayingCardView.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Lightricks. All rights reserved.
+// Created by Yossy Carmeli.
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayingCardView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Matchismo/View/SetCardView.mm
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Lightricks. All rights reserved.
+// Created by Yossy Carmeli.
+
+#import "SetCardView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SetCardView
+
+@end
+
+NS_ASSUME_NONNULL_END
